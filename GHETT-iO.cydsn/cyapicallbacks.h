aa02b741c67ca659ecf545e122a41f76a83643f5
Kate Gray <opensource@codebykate.com>
2017-10-13 21:25:04 -0400

    Revert "Merge"
    This reverts commit 1d4a38f636f7ddea482a7f6ff6a2283a342a6a9a, reversing
    changes made to b76b863a13c8c55200fefdd4a9e00c7362fcd2af.

--- GHETT-iO.cydsn/cyapicallbacks.h
+++ GHETT-iO.cydsn/cyapicallbacks.h
@@ -6,19 +6,10 @@
  * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
  */
 #ifndef CYAPICALLBACKS_H
-#include <cytypes.h>
     
-// Enable the USBFS endpoint 0 (control) callback
-#define USBFS_EP_0_ISR_EXIT_CALLBACK
-extern void USBFS_EP_0_ISR_ExitCallback();
-    
-// Enable the USBFS endpoint 1 (input) callback
+// Enable the USBFS endpoint callback
 #define USBFS_EP_1_ISR_EXIT_CALLBACK
 extern void USBFS_EP_1_ISR_ExitCallback();
-
-// Callback function for handling vendor requests
-#define USBFS_HANDLE_VENDOR_RQST_CALLBACK
-extern uint8 USBFS_HandleVendorRqst_Callback();
 
 #define CYAPICALLBACKS_H    
 #endif /* CYAPICALLBACKS_H */   
